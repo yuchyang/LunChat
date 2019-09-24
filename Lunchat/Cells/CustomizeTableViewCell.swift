e57b497cf5932200ba181eeb576dc09fc3ab9812
Yucheng yang <997471028@qq.com>
2019-09-24 22:17:38 +1000

    commit

--- Lunchat/Cells/CustomizeTableViewCell.swift
+++ Lunchat/Cells/CustomizeTableViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  CustomizeTableViewCell.swift
+//  ViewController.swift
 //  Lunchat
 //
 //  Created by 杨昱程 on 22/9/19.
@@ -8,23 +8,79 @@
 
 import UIKit
 
-class CustomizeTableViewCell: UIViewController {
 
+class MateViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    var dataSource = [[String:String]()]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let tableView = UITableView(frame: view.bounds, style: .grouped)
+        tableView.backgroundColor = UIColor.white;
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        dataSource = [
+            ["name":"Tom Marshall","sex":"male","icon":"no-user-image-square","department":"Master of Bussiness"],
+            ["name":"Pena Valdez","sex":"female","icon":"no-user-image-square","department":"Master of Computer Science"],
+            ["name":"Jessica","sex":"female","icon":"no-user-image-square","department":"Master of teaching"],
+            ["name":"JIM","sex":"male","icon":"no-user-image-square","department":"Master of Information system"]]
+        tableView.reloadData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: UITableViewDataSource
+    // cell的个数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
     }
-    */
-
+    // UITableViewCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellid = "testCellID"
+        var cell:MateTableViewCell? = tableView.dequeueReusableCell(withIdentifier: cellid) as? MateTableViewCell
+        if cell==nil {
+            cell = MateTableViewCell(style: .subtitle, reuseIdentifier: cellid)
+        }
+        let dict:Dictionary = dataSource[indexPath.row]
+        cell?.iconImv.image = UIImage(named: dict["icon"]!)
+        cell?.userLabel.text = dict["name"]
+//        cell?.sexLabel.text = dict["sex"]
+        cell?.departmentLabel.text = dict["department"]
+        return cell!
+    }
+    
+    //MARK: UITableViewDelegate
+    // 设置cell高度
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    // 选中cell后执行此方法
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    func currentTime() -> String {
+        
+        let dateformatter = DateFormatter()
+        
+        dateformatter.dateFormat = "YYYY-MM-dd"// 自定义时间格式
+        
+        // GMT时间 转字符串，直接是系统当前时间
+        
+        return dateformatter.string(from: Date())
+        
+    }
 }
+
